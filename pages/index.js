ecc3f33631b393ee3708c1e3322c68e5923e2c5b
Cleyton Seles <cleyttonselestj@gmail.com>
2026-09-07 18:27:32 -0300

    Configura EditorConfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-    function Home() {
-        return <h1>Espelho, espelho meu, existe alguém mais gay que Leonardinho boca de Jubileu?</h1>
-    }
+function Home() {
+  return <h1>Espelho, espelho meu, existe alguém mais gay que Leonardinho boca de Jubileu?</h1>
+}
 
-    export default Home;
+export default Home;
